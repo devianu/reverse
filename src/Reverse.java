df80f256d27bfce6e99969199db5c9648aba8b76
VAISHNAVI <VAISHNAVI@DESKTOP-BIAC4VH>
2021-07-05 16:09:47 +0530

    First commit....

---
+++ src/Reverse.java
@@ -0,0 +1,6 @@
+public class Reverse {  
+public static void main(String[] args) {  
+    System.out.println(StringFormatter.reverseWord("my name is khan"));  
+    System.out.println(StringFormatter.reverseWord("I am sonoo jaiswal"));    
+    }  
+}  
